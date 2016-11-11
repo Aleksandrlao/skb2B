8aec538ce6f1a48f62e953c8cc77e7f7569aedc9
Aleksandrlao <aleksandrlao@mail.ru>
2016-11-11 23:15:36 +0200

    first

--- src/index.js
+++ src/index.js
@@ -14,6 +14,47 @@ app.get('/aleksandrlao', (req, res) => {
 	res.send(sum.toString());
 });
 
+app.get('/aleksandrlao2', (req, res) => {
+	
+		
+
+	if( req.query.fullname ) {
+
+		var re = /\s/g;
+		var str = req.query.fullname;
+		var newstr = str.split(" ");
+
+		if( newstr.length > 3 ) {
+			var itog = "Invalid fullname";
+		} else if( newstr.length == 3 ) {
+			var itog = newstr[newstr.length - 1];
+			var otch = newstr[newstr.length - 2].slice(0,1);
+			var imya = newstr[newstr.length - 3].slice(0,1);
+			var itog = itog + " " + imya + ". " + otch + ".";
+		} else if( newstr.length == 2 ) {
+			var itog = newstr[newstr.length - 1];
+			var otch = newstr[newstr.length - 2].slice(0,1);
+			var itog = itog + " " + otch + ".";
+		} else if( newstr.length == 1 ) {
+			var itog = newstr[newstr.length - 1];
+		}
+		
+		
+
+		console.log(newstr);
+
+	} else {
+
+		var itog = "Invalid fullname";
+
+	}
+
+	
+	
+	
+	res.send(itog);
+});
+
 app.listen(3000, () => {
   console.log('Your app listening on port 3000!');
 });
